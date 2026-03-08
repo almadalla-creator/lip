5087d07ea7a12e8ccb3866a4b557878a2f66e7d9
almadalla-creator <almadalla@yahoo.com>
2026-03-08 19:22:13 +0400

    Update app.js

--- interactive/relationships/app.js
+++ interactive/relationships/app.js
@@ -5,7 +5,7 @@
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
 
-  const STORAGE_KEY = "lip_relashionships_choices";
+  const STORAGE_KEY = "lip_relationships_choices";
   let index = 0;
 
   // Mapping is explicit:
